b10d9d699f89de35b384f3d6c920b6ab42c6ee0a
thegecko <rob.moran@arm.com>
2020-05-31 12:42:17 +0100

    Add echo

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', ws => {
-    ws.on('message', message => console.log(`received: ${message}`));
-    ws.send('something');
+    ws.on('message', message => {
+        console.log(`received: ${message}, sending back...`);
+        ws.send(message);
+    });
 });
